743e36804ca102de4884fee71a2248d3768e5d20
putriitr <if322007@students.del.ac.id>
2024-06-19 14:50:43 +0700

    push portfolio

---
+++ my-portfolio/resources/views/contactform.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact Form Submission</title>
+</head>
+<body>
+    <h2>Contact Form Submission</h2>
+    <p>Name: {{ $name }}</p>
+    <p>Email: {{ $email }}</p>
+    <p>Subject: {{ $subject }}</p>
+    <p>Message:</p>
+    <p>{{ $message }}</p>
+</body>
+</html>
---
+++ my-portfolio/resources/views/partial/css.blade.php
@@ -0,0 +1,31 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Putri's Portfolio</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="author" content="" />
+<meta property="og:title" content="" />
+<meta property="og:image" content="" />
+<meta property="og:url" content="" />
+<meta property="og:site_name" content="" />
+<meta property="og:description" content="" />
+<meta name="twitter:title" content="" />
+<meta name="twitter:image" content="" />
+<meta name="twitter:url" content="" />
+<meta name="twitter:card" content="" />
+<link rel="shortcut icon" href="favicon.ico">
+
+<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+<link rel="stylesheet" href="{{ asset('css/icomoon.css') }}">
+<link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+<link rel="stylesheet" href="{{ asset('css/flexslider.css') }}">
+<link rel="stylesheet" href="{{ asset('fonts/flaticon/font/flaticon.css')}}">
+<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<script src="{{ asset('js/modernizr-2.6.2.min.js')}}"></script>
